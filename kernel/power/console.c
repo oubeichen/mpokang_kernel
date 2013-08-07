aab82c5c65356863485a01303ddf5f2017c4d851
Lawrence Z. <lorenzo_air@live.it>
2013-08-07 11:40:20 +0200

    Kernel console from 3.10 kernel

--- kernel/power/console.c
+++ kernel/power/console.c
@@ -1,8 +1,8 @@
 /*
- * drivers/power/process.c - Functions for saving/restoring console.
- *
- * Originally from swsusp.
- */
+* drivers/power/process.c - Functions for saving/restoring console.
+*
+* Originally from swsusp.
+*/
 
 #include <linux/vt_kern.h>
 #include <linux/kbd_kern.h>
@@ -11,31 +11,25 @@
 #include "power.h"
 
 #if defined(CONFIG_VT) && defined(CONFIG_VT_CONSOLE)
-#define SUSPEND_CONSOLE	(MAX_NR_CONSOLES-1)
+#define SUSPEND_CONSOLE (MAX_NR_CONSOLES-1)
 
 static int orig_fgconsole, orig_kmsg;
 
 int pm_prepare_console(void)
 {
-	orig_fgconsole = vt_move_to_console(SUSPEND_CONSOLE, 1);
-	if (orig_fgconsole < 0)
-		return 1;
+orig_fgconsole = vt_move_to_console(SUSPEND_CONSOLE, 1);
+if (orig_fgconsole < 0)
+return 1;
 
-	orig_kmsg = vt_kmsg_redirect(SUSPEND_CONSOLE);
-	return 0;
+orig_kmsg = vt_kmsg_redirect(SUSPEND_CONSOLE);
+return 0;
 }
 
 void pm_restore_console(void)
 {
-	if (orig_fgconsole >= 0) {
-		vt_move_to_console(orig_fgconsole, 0);
-		vt_kmsg_redirect(orig_kmsg);
-	}
+if (orig_fgconsole >= 0) {
+vt_move_to_console(orig_fgconsole, 0);
+vt_kmsg_redirect(orig_kmsg);
+}
 }
 #endif
-
-void pm_restore_console(void)
-{
-	if (orig_fgconsole >= 0) {
-		vt_move_to_console(orig_fgconsole, 0);
-		vt_kmsg_redirect(orig_kmsg);
